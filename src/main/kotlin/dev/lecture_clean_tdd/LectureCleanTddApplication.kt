7d0cc2fbd8aed5ebacd790eabeee13668344c426
juhwan <asdcz@naver.com>
2024-06-22 16:02:59 +0900

    Initial commit

---
+++ src/main/kotlin/dev/lecture_clean_tdd/LectureCleanTddApplication.kt
@@ -0,0 +1,11 @@
+package dev.lecture_clean_tdd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LectureCleanTddApplication
+
+fun main(args: Array<String>) {
+	runApplication<LectureCleanTddApplication>(*args)
+}
---
+++ src/test/kotlin/dev/lecture_clean_tdd/LectureCleanTddApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.lecture_clean_tdd
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LectureCleanTddApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
