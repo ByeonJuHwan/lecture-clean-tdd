6631260e5d3e40fc6ae7309484fd3a073e6fef78
juhwan <asdcz@naver.com>
2024-06-24 20:57:19 +0900

    infra layer 인터페이스 구현체 작성

---
+++ src/main/kotlin/dev/lecture_clean_tdd/adapter/persistence/LectureRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package dev.lecture_clean_tdd.adapter.persistence
+
+import dev.lecture_clean_tdd.Exception.LectureNotFoundException
+import dev.lecture_clean_tdd.adapter.persistence.jpa.JpaLectureRepository
+import dev.lecture_clean_tdd.application.port.output.LectureRepository
+import dev.lecture_clean_tdd.domain.entity.Lecture
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
+
+@Repository
+class LectureRepositoryImpl(
+    private val jpaLectureRepository: JpaLectureRepository,
+) : LectureRepository {
+
+    override fun save(lecture: Lecture): Long {
+        return jpaLectureRepository.save(lecture).id
+    }
+
+    override fun findById(lectureId: Long): Lecture? {
+        return jpaLectureRepository.findByIdOrNull(lectureId)
+    }
+
+    override fun findByIdWithLock(lectureId: Long): Lecture? {
+        return jpaLectureRepository.findByIdWithLock(lectureId)
+    }
+}
