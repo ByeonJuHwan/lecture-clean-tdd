d4948959114cd15a8f2b9c2e09abdb2a279d749d
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-06-27 22:10:42 +0900

    특강신청여부조회 비즈니스 로직

--- src/main/kotlin/dev/lecture_clean_tdd/application/service/LectureStatusService.kt
+++ src/main/kotlin/dev/lecture_clean_tdd/application/service/LectureStatusService.kt
@@ -10,26 +10,26 @@ import dev.lecture_clean_tdd.domain.entity.Lecture
 import dev.lecture_clean_tdd.domain.entity.User
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-
-@Service
-@Transactional(readOnly = true)
-class LectureStatusService (
-    private val lectureAttendeeRepository: LectureAttendeeRepository,
-    private val lectureRepository: LectureRepository,
-    private val userRepository : UserRepository,
-) : LectureStatusUseCase{
-    override fun isLectureRegistered(userId: Long, lectureId: Long): Boolean {
-        val user = getUser(userId)
-        val lecture = getLecture(lectureId)
-
-        return lectureAttendeeRepository.findByUserAndLecture(user, lecture)?.let { true } ?: false
-    }
-
-    private fun getUser(userId: Long): User {
-        return userRepository.findById(userId) ?: throw UserNotFoundException("유저를 찾을 수 없습니다")
-    }
-
-    private fun getLecture(lectureId: Long): Lecture {
-        return lectureRepository.findById(lectureId) ?: throw LectureNotFoundException("강의를 찾을 수 없습니다")
-    }
-}
+ 
+@Service 
+@Transactional(readOnly = true) 
+class LectureStatusService ( 
+    private val lectureAttendeeRepository: LectureAttendeeRepository, 
+    private val lectureRepository: LectureRepository, 
+    private val userRepository : UserRepository, 
+) : LectureStatusUseCase{ 
+    override fun isLectureRegistered(userId: Long, lectureId: Long): Boolean { 
+        val user = getUser(userId) 
+        val lecture = getLecture(lectureId) 
+ 
+        return lectureAttendeeRepository.findByUserAndLecture(user, lecture)?.let { true } ?: false 
+    } 
+ 
+    private fun getUser(userId: Long): User { 
+        return userRepository.findById(userId) ?: throw UserNotFoundException("유저를 찾을 수 없습니다") 
+    } 
+ 
+    private fun getLecture(lectureId: Long): Lecture { 
+        return lectureRepository.findById(lectureId) ?: throw LectureNotFoundException("강의를 찾을 수 없습니다") 
+    } 
+} 
