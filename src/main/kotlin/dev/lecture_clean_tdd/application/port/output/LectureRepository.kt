347fcdfcded302d077af705c2877b422bf15a162
ByeonJuHwan <105885581+ByeonJuHwan@users.noreply.github.com>
2024-06-27 22:19:19 +0900

    Lecture 비관적락 인터페이스

--- src/main/kotlin/dev/lecture_clean_tdd/application/port/output/LectureRepository.kt
+++ src/main/kotlin/dev/lecture_clean_tdd/application/port/output/LectureRepository.kt
@@ -5,6 +5,6 @@ import dev.lecture_clean_tdd.domain.entity.Lecture
 interface LectureRepository {
     fun save(lecture : Lecture) : Long
     fun findById(lectureId : Long) : Lecture?
-    fun findByIdWithLock(lectureId: Long): Lecture?
+    fun findByIdWithLock(lectureId: Long): Lecture? 
     fun findAll(): List<Lecture>
 }
